7788071f5b0c310f51571b4d6b70012586654782
Nathan Oh <nathanoh@Nathans-Laptop.local>
2024-05-02 20:39:34 -0400

    added 0.0.0.0 to allowed hosts

--- app/app/settings.py
+++ app/app/settings.py
@@ -123,3 +123,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ALLOWED_HOSTS = ["0.0.0.0"]
